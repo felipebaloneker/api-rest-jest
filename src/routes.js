4683bba4d0187733ba8db0b1776dfe8493afea7d
felipe baloneker <felipebaloneker23@gmail.com>
2022-05-24 15:38:24 -0300

    testing whether a user is created

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,10 @@
 const routes = require("express").Router();
+const {User} = require("./app/models");
+
+User.create({
+    name: 'Diego', 
+    email: 'diego@rocketseat.com.br',
+    password_hash: '198234712932131231'
+});
 
 module.exports = routes;
