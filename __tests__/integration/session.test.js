6657d534e42c9febe46f60214af626a5a1f0be74
felipe baloneker <felipebaloneker23@gmail.com>
2022-05-25 12:29:18 -0300

    starting tests from create user router

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,10 +1,13 @@
 const { User } = require('../../src/app/models');
-describe('Authentication',()=>{
-    it('should sum two numbers',()=>{
-       const user =  await User.create({name:'Felipe', email:'felipe@test.com', password_hash:'123123'})
 
-       console.log(user);
-
-       exprect(user.email).toBe('felipe@test.com')
-    });
+describe('Authentication',() => {
+   it('should authenticate with valid credentials', async ()=>{
+       const user = User.create({
+           name: "Felipe",
+           email: "felipe@test.com.br",
+            password_hash: "123123"
+       });
+       console.log(JSON.stringify(user, null, 4))
+    //    expect(user.email).toBe("felipe@test.com.br")
+   })
 })
--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,12 @@
 require("dotenv").config({
-  path: process.env.NODE_ENV === "test" ? "./env.test" : ".env"
+  path: process.env.NODE_ENV === "test" ? "./.env.test" : ".env"
 });
 module.exports = {
   host:process.env.DB_HOST,
   username:process.env.DB_USER,
   password:process.env.DB_PASS,
   database:process.env.DB_NAME,
-  dialect: process.env.DB_DIALECT || "postgres",
+  dialect: process.env.DB_DIALECT,
   storage: './__tests__/database.sqlite',
   logging: false,
   define:{
